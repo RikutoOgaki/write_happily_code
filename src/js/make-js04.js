eec54b2e06f74dd75bb4c6879466b9f3370cbd35
Rikuto <orikutobasuke@gmail.com>
2022-10-26 14:52:39 +0900

    make-js04 add

--- src/js/make-js04.js
+++ src/js/make-js04.js
@@ -1,20 +1,28 @@
 const box = document.querySelectorAll(".box");
+let tBox ;
+let num = 10;
 console.log(box);
 let flg = false
 box.forEach((e,i) => {
     console.log(e,i);
-    e.addEventListener('mousedown', () => {
+    e.addEventListener('mousedown', (evt) => {
+        console.log(evt.target);
+        tBox = evt.target;
         e.style.backgroundColor="skyblue"
-        flg = true
-        window.addEventListener('mousemove', (evt) => {
-            if(flg){
-                e.style.left = evt.clientX-62.5 + "px"
-                e.style.top = evt.clientY-62.5 + "px"
-            }
-        })
+        num = num+=1;
+        e.style.zIndex = num ;
+        flg = true;     
     e.addEventListener('mouseup', () =>{
             e.style.backgroundColor=""
             flg = false
     })
     })
+})
+
+window.addEventListener('mousemove', (evt) => {
+    console.log(evt);
+    if(flg){
+        tBox.style.left = evt.clientX-62.5 + "px"
+        tBox.style.top = evt.clientY-62.5 + "px"
+    }
 })
