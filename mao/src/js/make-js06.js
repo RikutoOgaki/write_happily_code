c6daa76f92efc60df74db62ae36b7509ac7b541f
Rikuto <orikutobasuke@gmail.com>
2022-10-28 11:27:35 +0900

    make-js06 add

--- mao/src/js/make-js06.js
+++ mao/src/js/make-js06.js
@@ -1,5 +1,16 @@
 const btn =  document.getElementById("btn");
+const tet = document.getElementById("text");
 console.log(btn);
+console.log(tet);
+let flg = true;
 btn.addEventListener('click' , () => {
-    
+    tet.innerHTML = "ここにでてくるよ";
+    if(flg == true){
+        tet.innerHTML = "押された";
+        flg = false
+    }
+    else if(flg == false){
+        tet.innerHTML = "もっかい押された";
+        flg = true
+    }
 });
