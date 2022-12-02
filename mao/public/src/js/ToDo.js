2ff2fddf60902460f76e3005d60c3c132ca8db3d
Rikuto <orikutobasuke@gmail.com>
2022-12-02 12:29:43 +0900

    fill add

---
+++ mao/public/src/js/ToDo.js
@@ -0,0 +1,10 @@
+// list() {
+    const form = document.getElementById("js-form");
+    const input = document.getElementById("text-js");
+    // console.log(input.value);
+    form.addEventListener("submit",(evt)=>{
+        console.log("hello");
+        // evt.preventDefault
+        // console.log(`入力値の値 ${input.value}`);
+    });
+// }
