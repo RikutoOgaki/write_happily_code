cd8b9bbb18c68558fa03619b7235f109d261410c
Rikuto <orikutobasuke@gmail.com>
2022-12-06 15:48:07 +0900

    map study add

--- mao2/public/src/js/sample02.js
+++ mao2/public/src/js/sample02.js
@@ -1,2 +1,28 @@
-const map = new Map();
-console.log(map.size);
+{
+    const map = new Map();
+    console.log(map.size);
+}
+
+{
+    const map = new Map();
+    // 新しい要素の追加
+    map.set("key","value1")
+    console.log(map.size);
+    console.log(map.get("key"));
+    // 要素の上書き
+    map.set("key","value2")
+    console.log(map.get("key"));
+    // キーの存在
+    console.log(map.has("key"));
+    console.log(map.has("foo"));
+}
+
+{
+   // mapの反復処理
+   const map = new Map([["key1","value1"],["key2","value2"]]);
+   const result = [];
+   map.forEach((value,key)=>{
+    result.push(`${key}:${value}`)
+   });
+   console.log(result);
+}
